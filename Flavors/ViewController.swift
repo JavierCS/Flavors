7ace97fe88a9fbec77c569eec0c49288656e0c65
Javier Cruz Santiago <javier.cruzs@bancoazteca.com>
2020-06-03 15:44:53 -0500

    Initial Commit

---
+++ Flavors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flavors
+//
+//  Created by Javier Cruz Santiago on 03/06/20.
+//  Copyright © 2020 Banco Azteca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
