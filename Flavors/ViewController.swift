af1e7404a8f831650dfc96b1158e38b740177cc1
Javier Cruz Santiago <javier.cruzs@bancoazteca.com>
2020-06-17 16:54:13 -0500

    [INITIAL] - Initial commit

--- Flavors/ViewController.swift
+++ Flavors/ViewController.swift
@@ -13,8 +13,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        #if DEBUGTwo
+        self.title = "Two"
+        #elseif DEBUG
+        self.title = "One"
+        #endif
     }
-
-
 }
 
